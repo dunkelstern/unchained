3051d74e7685c84d2d367071b12257821cea4e96
Johannes Schriewer (privat) <dark@p200300625d3097afaebc32fffea8087b.dip0.t-ipconnect.de>
2015-11-30 23:08:42 +0100

    Initial Commit

---
+++ unchainedTests/unchainedTests.swift
@@ -0,0 +1,36 @@
+//
+//  unchainedTests.swift
+//  unchainedTests
+//
+//  Created by Johannes Schriewer (privat) on 30/11/15.
+//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//
+
+import XCTest
+@testable import unchained
+
+class unchainedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
