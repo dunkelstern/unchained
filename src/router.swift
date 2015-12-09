b159ddfd2c9a795ac5ab74cfb0087fd372707097
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-09 01:58:20 +0100

    Implement router

--- src/router.swift
+++ src/router.swift
@@ -8,11 +8,12 @@
 
 import Darwin
 import twohundred
+import SwiftyRegex
 
 public struct UnchainedRoute {
-    public typealias RequestHandler = ((request: HTTPRequest, parameters: [String:String]) -> HTTPResponse)
+    public typealias RequestHandler = ((request: HTTPRequest, parameters: [String], namedParameters: [String:String]) -> HTTPResponse)
     
-    private var regexString: String
+    private var re: RegEx
     private var name: String?
     private var handler:RequestHandler
     
@@ -21,8 +22,8 @@ public struct UnchainedRoute {
     /// - parameter regex: Regex to match
     /// - parameter handler: handler callback to run if route matches
     /// - parameter name: (optional) name of this route to `reverse`
-    public init(_ regex: String, handler:RequestHandler, name: String? = nil) {
-        self.regexString = regex
+    public init(_ regex: String, handler:RequestHandler, name: String? = nil) throws {
+        self.re = try RegEx(pattern: regex)
         self.name = name
         self.handler = handler
     }
@@ -32,16 +33,10 @@ public struct UnchainedRoute {
     /// - parameter request: the request on which to execute this route
     /// - returns: response to the request or nil if the route does not match
     public func execute(request: HTTPRequest) -> HTTPResponse? {
-        if self.doesMatch(request.header.url) {
-            // TODO: Split parameters by regex
-            return self.handler(request: request, parameters: [String:String]())
+        let matches = self.re.match(request.header.url)
+        if matches.numberedParams.count > 0 {
+            return self.handler(request: request, parameters: matches.numberedParams, namedParameters: matches.namedParams)
         }
         return nil
-    }
-    
-    // MARK: - Private
-    private func doesMatch(url: String) -> Bool {
-        // TODO: Regex match the url to determine match
-        return false
     }
 }
