05c26ef4dbc363fda25b340118e1a9fd31b21bcb
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-09 03:37:09 +0100

    Router does not throw anymore but disables the defect route and logs a warning

--- src/router.swift
+++ src/router.swift
@@ -10,10 +10,13 @@ import Darwin
 import twohundred
 import SwiftyRegex
 
+/// Unchained route entry
 public struct UnchainedRoute {
+    
+    /// A route request handler, takes `request`, numbered `parameters` and `namedParameters`, returns `HTTPResponse`
     public typealias RequestHandler = ((request: HTTPRequest, parameters: [String], namedParameters: [String:String]) -> HTTPResponse)
     
-    private var re: RegEx
+    private var re: RegEx?
     private var name: String?
     private var handler:RequestHandler
     
@@ -22,8 +25,15 @@ public struct UnchainedRoute {
     /// - parameter regex: Regex to match
     /// - parameter handler: handler callback to run if route matches
     /// - parameter name: (optional) name of this route to `reverse`
-    public init(_ regex: String, handler:RequestHandler, name: String? = nil) throws {
-        self.re = try RegEx(pattern: regex)
+    public init(_ regex: String, handler:RequestHandler, name: String? = nil) {
+        do {
+            self.re = try RegEx(pattern: regex)
+        } catch RegEx.Error.InvalidPattern(let offset, let message) {
+            // TODO: Implement centralized logging
+            print("UnchainedRoute: Pattern parse error for pattern \(regex) at character \(offset): \(message)")
+        } catch {
+            // unused
+        }
         self.name = name
         self.handler = handler
     }
@@ -33,7 +43,10 @@ public struct UnchainedRoute {
     /// - parameter request: the request on which to execute this route
     /// - returns: response to the request or nil if the route does not match
     public func execute(request: HTTPRequest) -> HTTPResponse? {
-        let matches = self.re.match(request.header.url)
+        guard let re = self.re else {
+            return nil
+        }
+        let matches = re.match(request.header.url)
         if matches.numberedParams.count > 0 {
             return self.handler(request: request, parameters: matches.numberedParams, namedParameters: matches.namedParams)
         }
