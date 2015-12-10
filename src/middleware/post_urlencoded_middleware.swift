c6ed397aaa1c729e9509606f872a73faaacd99b8
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-10 23:43:09 +0100

    Add URL decoding for POST with application/x-www-form-urlencoded (middleware)

---
+++ src/middleware/post_urlencoded_middleware.swift
@@ -0,0 +1,68 @@
+//
+//  post_urlencoded_middleware.swift
+//  unchained
+//
+//  Created by Johannes Schriewer on 10/12/15.
+//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//
+
+import twohundred
+
+/// URL Decoding POST data middleware
+public class URLEncodedPostMiddleware: UnchainedMiddleware {
+    public init() {}
+    
+    public func request(request: HTTPRequest, config: UnchainedConfig) -> (request: HTTPRequest?, response: HTTPResponse?)? {
+        guard request.header["content-type"] == "application/x-www-form-urlencoded" &&
+              request.data != nil &&
+              request.data!.count > 0 else {
+            return nil // not our case
+        }
+        var modifiedRequest = request
+        
+        // zero terminate data
+        modifiedRequest.data!.append(0)
+        
+        // make string from request body
+        if let body = String(CString: UnsafePointer<Int8>(modifiedRequest.data!), encoding: NSUTF8StringEncoding) {
+            
+            // split tuples
+            let tuples = body.componentsSeparatedByString("&")
+        
+            // create postData dict
+            var postData = [String:String]()
+            for item in tuples {
+                let parts = item.componentsSeparatedByString("=")
+                if parts.count == 2 {
+                    postData[parts[0].urlDecodedString()] = parts[1].urlDecodedString()
+                }
+            }
+            
+            // save to postData
+            modifiedRequest.postData = postData
+            
+            // return modified request
+            return (request: modifiedRequest, response: nil)
+        }
+        return nil
+    }
+}
+
+/// PostData extension for HTTPRequest
+extension HTTPRequest {
+    
+    /// Decoded POST data
+    public var postData:[String:String]? {
+        
+        get {
+            if case let postData as [String:String] = self.middlewareData["postData"] {
+                return postData
+            }
+            return nil
+        }
+        
+        set(newValue) {
+            self.middlewareData["postData"] = newValue
+        }
+    }
+}
