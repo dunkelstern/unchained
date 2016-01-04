cf23c417c5f20bdec5fa02122df8d2aeb4bab8ea
Johannes Schriewer <hallo@dunkelstern.de>
2016-01-04 19:32:17 +0100

    Use 1.x of SwiftyRegex

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 		.Package(url:"https://github.com/dunkelstern/DEjson.git", majorVersion: 1),
 		.Package(url:"https://github.com/dunkelstern/Base64.git", majorVersion: 0),
 		.Package(url:"https://github.com/dunkelstern/QuotedPrintable.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/SwiftyRegex.git", majorVersion: 0),
+		.Package(url:"https://github.com/dunkelstern/SwiftyRegex.git", majorVersion: 1),
 		.Package(url:"https://github.com/dunkelstern/Stencil.git", majorVersion: 0),
 		.Package(url:"https://github.com/dunkelstern/UnchainedUUID.git", majorVersion: 0),
 		.Package(url:"https://github.com/dunkelstern/UnchainedGlibc.git", majorVersion: 0)
