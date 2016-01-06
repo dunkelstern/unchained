1ec95bb54f600613f074a83e59fc943871a47368
Johannes Schriewer <hallo@dunkelstern.de>
2016-01-06 03:46:22 +0100

    Fix Package.swift, swift package manager seems to fail to resolve the complete dependency tree if unchained is used as dependency

--- Package.swift
+++ Package.swift
@@ -16,13 +16,27 @@ let package = Package(
         Target(name:"Unchained")
 	],
 	dependencies: [
-		.Package(url:"https://github.com/dunkelstern/twohundred.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/DEjson.git", majorVersion: 1),
-		.Package(url:"https://github.com/dunkelstern/Base64.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/QuotedPrintable.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/SwiftyRegex.git", majorVersion: 1),
-		.Package(url:"https://github.com/dunkelstern/Stencil.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/UnchainedUUID.git", majorVersion: 0),
-		.Package(url:"https://github.com/dunkelstern/UnchainedGlibc.git", majorVersion: 0)
-	]
+        .Package(url:"https://github.com/dunkelstern/Adler32.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedString.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedDate.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedFile.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedLogger.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedIPAddress.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/libUnchainedSocket.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/twohundred.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/DEjson.git", majorVersion: 1),
+        .Package(url:"https://github.com/dunkelstern/Base64.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/QuotedPrintable.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/pcre.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/SwiftyRegex.git", majorVersion: 1),
+        .Package(url:"https://github.com/dunkelstern/Stencil.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedUUID.git", majorVersion: 0)
+    ]
 )
+
+#if os(Linux)
+    package.dependencies.appendContentsOf([
+        .Package(url:"https://github.com/dunkelstern/BlocksRuntime.git", majorVersion: 0),
+        .Package(url:"https://github.com/dunkelstern/UnchainedGlibc.git", majorVersion: 0)
+    ])
+#endif
